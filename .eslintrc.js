866202624b8490256e67991970695d5615377fbf
Peter Placzek <peter.placzek1996@gmail.com>
2020-04-15 19:45:23 +0200

    updated editor & build files

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    //'plugin:nuxt/recommended'
+  ],
+  // add your custom rules here
+  rules: {
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    "semi": 0,
+    'no-console': 'off',
+    'spaced-comment': 'off'
+  }
+}
