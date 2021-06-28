6c6f86f04b0514455c97905d87abef48c96383cc
Peter Placzek <peter.placzek1996@gmail.com>
2021-06-28 19:07:45 +0200

    result download file

--- packages/result-service/src/app/controllers/train/result/TrainResultController.ts
+++ packages/result-service/src/app/controllers/train/result/TrainResultController.ts
@@ -1,21 +1,35 @@
 import fs from "fs";
-
 import {getTrainResultFilePath} from "../../../../modules/pht/result/image";
 
 export async function getTrainResultRouteHandler(req: any, res: any) {
+    const { downloadId } = req.query;
     const { id } = req.params;
 
     if (typeof id !== 'string') {
         return res._failNotFound();
     }
 
-    const trainResultFilePath = getTrainResultFilePath(id);
+    let resultFileId : string = id;
+
+    if(typeof downloadId === 'string') {
+        resultFileId = downloadId;
+    }
+
+    const trainResultFilePath = getTrainResultFilePath(resultFileId);
+    const downloadFileName : string = id+'.tar';
 
     return fs.access(trainResultFilePath, fs.constants.F_OK | fs.constants.R_OK, (err) => {
         if(err) {
             return res._failNotFound({message: 'A train result which is identified by the provided identifier doesn\'t exist'});
         } else {
-            return res.download(trainResultFilePath, id+'.tar');
+            const stream = fs.createReadStream(trainResultFilePath);
+
+            res.set({
+                'Content-Disposition': `attachment; filename=${downloadFileName}`,
+                'Content-Type': 'application/pdf',
+            });
+
+            stream.pipe(res);
         }
     });
 }
