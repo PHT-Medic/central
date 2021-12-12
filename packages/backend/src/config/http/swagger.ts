1dabfdb43ab22888be1f58ae4d5a6a28965b704c
Peter <peter.placzek1996@gmail.com>
2021-12-12 10:08:48 +0100

    simplified swagger generation + updated docker worklow

--- packages/backend/src/config/http/swagger.ts
+++ packages/backend/src/config/http/swagger.ts
@@ -10,70 +10,62 @@ import {
     Specification,
     SwaggerDocFormatData,
     SwaggerDocFormatType,
-
-    createMetadataGenerator,
-    createSpecGenerator,
+    generateDocumentation,
 } from '@trapi/swagger';
 import path from 'path';
 import { getRootDirPath, getWritableDirPath } from '../paths';
 import env from '../../env';
 
-// tslint:disable-next-line:no-var-requires
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('../../../package.json');
-// tslint:disable-next-line:no-var-requires
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const tsConfig = require('../../../tsconfig.json');
+export async function generateSwaggerDocumentation() : Promise<Record<SwaggerDocFormatType, SwaggerDocFormatData>> {
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require,import/no-dynamic-require
+    const packageJson = require(path.join(getRootDirPath(), 'package.json'));
+    // eslint-disable-next-line @typescript-eslint/no-var-requires,global-require,import/no-dynamic-require
+    const tsConfig = require(path.join(getRootDirPath(), 'tsconfig.json'));
 
-const metadataConfig : MetadataConfig = {
-    entryFile: path.join(getRootDirPath(), 'src', 'app', 'controllers', '**', '*.ts'),
-    ignore: ['**/node_modules/**'],
-    decorator: {
-        internal: true,
-        library: [
-            'decorators-express',
-        ],
-    },
-};
-
-export const swaggerConfig : Specification.Config = {
-    yaml: true,
-    host: env.apiUrl,
-    name: 'Central UI - API Documentation',
-    description: packageJson.description,
-    basePath: '/',
-    version: packageJson.version,
-    outputDirectory: getWritableDirPath(),
-    securityDefinitions: {
-        bearer: {
-            name: 'Bearer',
-            type: 'apiKey',
-            in: 'header',
+    const metadataConfig : MetadataConfig = {
+        entryFile: path.join(getRootDirPath(), 'src', 'app', 'controllers', '**', '*.ts'),
+        ignore: ['**/node_modules/**'],
+        decorator: {
+            internal: true,
+            library: [
+                'decorators-express',
+            ],
         },
-        oauth2: {
-            type: 'oauth2',
-            flows: {
-                password: {
-                    tokenUrl: `${env.apiUrl}token`,
+    };
+
+    const swaggerConfig : Specification.Config = {
+        yaml: true,
+        host: env.apiUrl,
+        name: 'UI - API Documentation',
+        description: packageJson.description,
+        basePath: '/',
+        version: packageJson.version,
+        outputDirectory: getWritableDirPath(),
+        securityDefinitions: {
+            bearer: {
+                name: 'Bearer',
+                type: 'apiKey',
+                in: 'header',
+            },
+            oauth2: {
+                type: 'oauth2',
+                flows: {
+                    password: {
+                        tokenUrl: `${env.apiUrl}token`,
+                    },
                 },
             },
+            basicAuth: {
+                type: 'http',
+                schema: 'basic',
+            },
         },
-        basicAuth: {
-            type: 'http',
-            schema: 'basic',
-        },
-    },
-    consumes: ['application/json'],
-    produces: ['application/json'],
-};
+        consumes: ['application/json'],
+        produces: ['application/json'],
+    };
 
-export async function generateSwaggerDocumentation() : Promise<Record<SwaggerDocFormatType, SwaggerDocFormatData>> {
-    const metadataGenerator = createMetadataGenerator(metadataConfig, tsConfig);
-
-    const metadata = metadataGenerator.generate();
-
-    const specGenerator = createSpecGenerator(metadata, swaggerConfig);
-
-    specGenerator.build();
-    return specGenerator.save();
+    return generateDocumentation({
+        metadata: metadataConfig,
+        swagger: swaggerConfig,
+    }, tsConfig);
 }
