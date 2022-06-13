cec6d407b52e42a9bbfbd3707e59186e404a980d
tada5hi <peter.placzek1996@gmail.com>
2022-06-13 12:52:01 +0200

    revert nanoid version

--- packages/backend/api/src/database/migrations/1655116530376-Default.ts
+++ packages/backend/api/src/database/migrations/1655116530376-Default.ts
@@ -1,6 +1,8 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+
 export class Default1655116530376 implements MigrationInterface {
     name = 'Default1655116530376';
+
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             ALTER TABLE \`registry_projects\`
@@ -11,6 +13,7 @@ export class Default1655116530376 implements MigrationInterface {
             ADD CONSTRAINT \`FK_0fadd5dc84495e2be1d98f22628\` FOREIGN KEY (\`realm_id\`) REFERENCES \`registries\`(\`id\`) ON DELETE CASCADE ON UPDATE NO ACTION
         `);
     }
+
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             ALTER TABLE \`registry_projects\` DROP FOREIGN KEY \`FK_0fadd5dc84495e2be1d98f22628\`
