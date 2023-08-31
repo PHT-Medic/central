1646d8a07c2cd227f2f2113bbda0b74b98f0f2ec
tada5hi <peter.placzek1996@gmail.com>
2023-08-31 13:47:14 +0200

    fix: dont return unknown property in setup routine

--- packages/client-ui/pages/login.vue
+++ packages/client-ui/pages/login.vue
@@ -65,7 +65,6 @@ export default defineNuxtComponent({
         return {
             handleDone,
             handleFailed,
-            translateValidationMessage,
             buildIdentityProviderURL,
         };
     },
